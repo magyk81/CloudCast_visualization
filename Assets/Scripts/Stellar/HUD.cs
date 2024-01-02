347d08038f9f1d8cac3d7da2e59fa3def2c23422
Joel Castellanos <joel@stellarscience.com>
2024-01-02 09:19:34 -0700

    replaced magic numbers in HUD with defined constants

--- Assets/Scripts/Stellar/HUD.cs
+++ Assets/Scripts/Stellar/HUD.cs
@@ -18,13 +18,18 @@ public class HUD : MonoBehaviour
     {
         // 1 degree longitude at 40 degrees North latitude is 85 km
         // 1 degree longitude at 45 degrees North latitude is 79 km
-        // 1 degree latitude at 111 km
+        float CENTER_LONGITUDE = 42.5f;
+        float CENTER_LATITUDE = -75.0f;
+        float METERS_PER_DEG_LONG = 111111f;
+        float z0 = 180.6f;
+        float x0 = -406.3f;
+        float y0 = 0f;
+
         Vector3 current_loc = Camera.main.transform.position;
-        float lat = 42.5f + (current_loc.z - 180.6f) * (1.0f / 111000.0f);
-        float lng_weight = (lat - 40f) / 5f;
-        float lng_scale = 85000.0f * (1.0f - lng_weight) + (79000.0f * lng_weight);
-        float lng = 75.0f - (current_loc.x + 406.3f) * (1.0f / lng_scale);
-        float alt = (current_loc.y + 100.0f);
+        float lat = CENTER_LONGITUDE + (current_loc.z - z0) * (1.0f / METERS_PER_DEG_LONG);
+        float meters_per_degree_lat = METERS_PER_DEG_LONG * Mathf.Cos(lat * Mathf.Deg2Rad);
+        float lng = -CENTER_LATITUDE - (current_loc.x - x0) * (1.0f / meters_per_degree_lat);
+        float alt = (current_loc.y - y0);
 
         string latStr = String.Format("{0:.000}", lat) + "°N";
         string lngStr = String.Format("{0:.000}", lng) + "°W";
